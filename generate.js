d9dcaf8ae6b6c052d71db52c083ac9e8572f1679
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-01-22 20:04:52 -0800

    handshake-names

---
+++ generate.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const BLACKLIST = require('./names/blacklist.json');
+const CUSTOM = require('./names/custom.json');
+const TLD = require('./names/tld.json');
+const CCTLD = require('./names/cctld.json');
+const GTLD = require('./names/gtld.json');
+const ALEXA = require('./names/alexa.json');
+
+function compile() {
+  const table = Object.create(null);
+  const names = [];
+
+  const insert = (name, tld, rank) => {
+    assert(!table[name]);
+    const data = [name, tld, rank, 0];
+    table[name] = data;
+    names.push(data);
+  };
+
+  for (const name of TLD)
+    insert(name, '', 0);
+
+  for (const name of CUSTOM)
+    insert(name, '', 0);
+
+  for (const name of CCTLD)
+    insert(name, '', 0);
+
+  assert(ALEXA.length >= 100000);
+
+  for (let i = 0; i < 100000; i++) {
+    const domain = ALEXA[i];
+    const parts = domain.split('.');
+    const rank = i + 1;
+
+    assert(parts.length >= 2);
+
+    const name = parts.shift();
+    assert(name.length > 0);
+
+    // Single letter domains only
+    // reserved for the alexa top 1,000.
+    if (rank > 1000) {
+      if (name.length === 1)
+        continue;
+    }
+
+    // Ignore deeply nested domains.
+    if (parts.length > 2)
+      continue;
+
+    // Third-level domain.
+    if (parts.length === 2) {
+      const [sld, tld] = parts;
+
+      // Country Codes only (e.g. co.uk).
+      if (tld.length !== 2)
+        continue;
+
+      // The SLD must be a known TLD (or `co`).
+      switch (sld) {
+        case 'com':
+        case 'org':
+        case 'net':
+        case 'edu':
+        case 'gov':
+        case 'mil':
+        case 'co':
+          break;
+        default:
+          continue;
+      }
+    }
+
+    const tld = parts.join('.');
+
+    // Cannot be longer than 64 bytes.
+    if (name.length > 64)
+      continue;
+
+    // Cannot contain non alphanumeric characters.
+    if (!/^[a-z0-9\-_]+$/.test(name))
+      continue;
+
+    // Cannot have leading/trailing dashes/underscores.
+    if (/^[\-_]|[\-_]$/.test(name))
+      continue;
+
+    // Ignore blacklist.
+    if (BLACKLIST.indexOf(name) !== -1)
+      continue;
+
+    // Check for collisions.
+    const item = table[name];
+
+    if (item) {
+      // Collision (keep the higher ranked one).
+      item[3] += 1;
+      continue;
+    }
+
+    insert(name, tld, rank);
+  }
+
+  // Sort lexicographically.
+  names.sort(([a], [b]) => {
+    return a.localeCompare(b);
+  });
+
+  return names;
+}
+
+/*
+ * Execute
+ */
+
+const names = compile();
+
+let out = '';
+
+out += '\'use strict\';\n';
+out += '\n';
+out += '// Format:\n';
+out += '// [name]: [[owner-tld], [alexa-rank], [collisions]]\n';
+out += 'module.exports = {\n';
+
+for (const [name, tld, rank, collisions] of names)
+  out += `  '${name}': ['${tld}', ${rank}, ${collisions}],\n`;
+
+out += '};\n';
+
+fs.writeFileSync(path.resolve(__dirname, 'names.js'), out);
---
+++ update.js
@@ -0,0 +1,149 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// TLD Resources:
+// https://www.icann.org/resources/pages/tlds-2012-02-25-en
+// https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+
+// SLD Resources:
+// https://www.google.com/search?q=alexa+top+100000
+// https://www.quora.com/What-are-the-top-100-000-most-visited-websites
+// http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const TLD_PATH = path.resolve(__dirname, 'data', 'tlds-alpha-by-domain.txt');
+const ALEXA_PATH = path.resolve(__dirname, 'data', 'top-1m.csv');
+
+const BLACKLIST = [
+  'example',
+  'local',
+  'localhost',
+  'onion'
+];
+
+const CUSTOM = [
+  'bit',
+  'eth'
+];
+
+const TLD = [
+  'arpa',
+  'com',
+  'edu',
+  'gov',
+  'int',
+  'mil',
+  'net',
+  'org'
+];
+
+const CCTLD = (() => {
+  const data = fs.readFileSync(TLD_PATH, 'utf8');
+  const lines = data.split('\n');
+  const result = [];
+
+  for (const line of lines) {
+    const name = line.trim().toLowerCase();
+
+    if (name.length === 0)
+      continue;
+
+    if (name[0] === '#')
+      continue;
+
+    assert(name.length <= 63);
+
+    // ccTLDs only!
+    if (name.length === 2)
+      result.push(name);
+  }
+
+  return result;
+})();
+
+const GTLD = (() => {
+  const data = fs.readFileSync(TLD_PATH, 'utf8');
+  const lines = data.split('\n');
+  const result = [];
+
+  for (const line of lines) {
+    const name = line.trim().toLowerCase();
+
+    if (name.length === 0)
+      continue;
+
+    if (name[0] === '#')
+      continue;
+
+    assert(name.length <= 63);
+
+    // gTLDs only!
+    if (name.startsWith('xn--'))
+      continue;
+
+    // gTLDs only!
+    if (name.length !== 2 && TLD.indexOf(name) === -1)
+      result.push(name);
+  }
+
+  return result;
+})();
+
+const ALEXA = (() => {
+  const data = fs.readFileSync(ALEXA_PATH, 'utf8');
+  const lines = data.split('\n');
+  const result = [];
+
+  let cur = 1;
+
+  for (const line of lines) {
+    const ln = line.trim().toLowerCase();
+
+    if (ln.length === 0)
+      continue;
+
+    const items = ln.split(/\s*,\s*/);
+    assert(items.length === 2);
+
+    const [num, domain] = items;
+    const rank = parseInt(num, 10);
+
+    assert((rank >>> 0) === rank);
+    assert(rank === cur);
+
+    result.push(domain);
+    cur += 1;
+  }
+
+  assert(result.length === 1000000);
+
+  return result;
+})();
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'blacklist.json'),
+  JSON.stringify(BLACKLIST, null, 2));
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'custom.json'),
+  JSON.stringify(CUSTOM, null, 2));
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'tld.json'),
+  JSON.stringify(TLD, null, 2));
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'cctld.json'),
+  JSON.stringify(CCTLD, null, 2));
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'gtld.json'),
+  JSON.stringify(GTLD, null, 2));
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'names', 'alexa.json'),
+  JSON.stringify(ALEXA, null, 2));
